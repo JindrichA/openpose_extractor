df011d4a470001d50d415a79da257cfd59fa7d3d
adolfjin <jindrich.adolf@gmail.com>
2023-10-30 13:09:43 +0100

    path of output folder changed

--- main_structurized.py
+++ main_structurized.py
@@ -135,96 +135,100 @@ def process_video_files(input_folder, output_folder, opWrapper, platform):
     print(f"Number of files to be processed: {len(content_input_folder)}")
 
     for name_of_file_and_exercise in tqdm(content_input_folder):
-        path_to_video = os.path.join(input_folder, name_of_file_and_exercise)
-        temp_folder_name = name_of_file_and_exercise[:-4]
 
-
-        final_file_name = os.path.join(output_folder, temp_folder_name+"_skeleton_compre.mp4")
-
-        if os.path.exists(final_file_name):
-            print(f"The file {final_file_name} is already processed, we will skip to another file.")
-            continue
-        else:
-            print(f"The file {final_file_name} is not processed yet, processing will begin now")
-
-
-
-
-        cam = cv2.VideoCapture(path_to_video)
-
-        fps = cam.get(cv2.CAP_PROP_FPS)
-        width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        frame_count = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
-        duration = int(frame_count / fps)
-
-        print(f'fps = {fps}, Number of frames = {frame_count}, Duration (S) = {duration}')
-
-        datum = op.Datum()
-        ret, imageToProcess = cam.read()
-        datum.cvInputData = imageToProcess
-        opWrapper.emplaceAndPop([datum])
-
-        full_25body_keypoints_data = np.full((frame_count, 25, 3), np.nan)
-
-        os.makedirs(os.path.join(output_folder, temp_folder_name), exist_ok=True)
-
-        for i in tqdm(range(frame_count - 1)):
-            try:
-                img = datum.cvOutputData.copy()
-                filename = os.path.join(output_folder, temp_folder_name, f"{str(i).zfill(6)}_{temp_folder_name}.jpg")
-            except Exception as e:
-                    print(f"An error occurred: {e}")
-
-
-            try:
-                ret, imageToProcess = cam.read()
-                datum.cvInputData = imageToProcess
-                opWrapper.emplaceAndPop([datum])
-                img = datum.cvOutputData.copy()
-                if datum.poseKeypoints is not None and datum.poseKeypoints.shape == (1, 25, 3):
-
-                    full_25body_keypoints_data[i, :, :] = datum.poseKeypoints[0]
-                    full_25body_keypoints_data[i][full_25body_keypoints_data[i] == 0] = np.nan
-                cv2.imwrite(filename, img)
-
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                #img = 255 * np.ones((int(height), int(width), 3), np.uint8)
-                #img = imageToProcess.copy()
-                # Scale the font size based on the image's dimensions
-                # Here, we scale it based on the width; you can adjust as needed
-                fontScale = width / 1000.0  # This is an arbitrary scaling factor; you might want to adjust it
-
-                # Get the size of the text box
-                text = 'No detected person in this frame'
-                (textWidth, textHeight), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale, 4)
-
-                # Compute starting position for the text so it's centered
-                x = int((width - textWidth) // 2)
-                y = int((height + textHeight) // 2)
-
-                # Place the text on the image
-                # Place the text on the image
-                cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 0, 255), 2)
-                cv2.imwrite(filename, img)
-                print(f"Frame n.o.: {i} can't be processed.")
-
-        numpy_xyc_matrix = np.array(full_25body_keypoints_data)
-        print(f"Finished Openpose for file: {temp_folder_name}")
-
-        scipy.io.savemat(os.path.join(output_folder, f"{temp_folder_name}.mat"),
-                         mdict={'xbody': numpy_xyc_matrix[:, :, 0], 'ybody': numpy_xyc_matrix[:, :, 1], 'cbody': numpy_xyc_matrix[:, :, 2]})
-        np.save(os.path.join(output_folder, f"{temp_folder_name}.npy"), numpy_xyc_matrix)
-
-        df_xyc_25_keypoints = create_dataframe(numpy_xyc_matrix, KEYPOINTS_NAMES)
-        df_xyc_25_keypoints.to_csv(os.path.join(output_folder, f"{temp_folder_name}.csv"), index=False)
-        # Saving as HDF5 (more space efficient and faster for large datasets)
         try:
-            df_xyc_25_keypoints.to_hdf(os.path.join(output_folder, f"{temp_folder_name}.h5"), key='keypoints', mode='w')
+
+
+            path_to_video = os.path.join(input_folder, name_of_file_and_exercise)
+            temp_folder_name = name_of_file_and_exercise[:-4]
+
+
+            final_file_name = os.path.join(output_folder, temp_folder_name+"_skeleton_compre.mp4")
+
+            if os.path.exists(final_file_name):
+                print(f"The file {final_file_name} is already processed, we will skip to another file.")
+                continue
+            else:
+                print(f"The file {final_file_name} is not processed yet, processing will begin now")
+            cam = cv2.VideoCapture(path_to_video)
+
+            fps = cam.get(cv2.CAP_PROP_FPS)
+            width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+            height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            frame_count = int(cam.get(cv2.CAP_PROP_FRAME_COUNT))
+            duration = int(frame_count / fps)
+
+            print(f'fps = {fps}, Number of frames = {frame_count}, Duration (S) = {duration}')
+
+            datum = op.Datum()
+            ret, imageToProcess = cam.read()
+            datum.cvInputData = imageToProcess
+            opWrapper.emplaceAndPop([datum])
+
+            full_25body_keypoints_data = np.full((frame_count, 25, 3), np.nan)
+
+            os.makedirs(os.path.join(output_folder, temp_folder_name), exist_ok=True)
+
+            for i in tqdm(range(frame_count - 1)):
+                try:
+                    img = datum.cvOutputData.copy()
+                    filename = os.path.join(output_folder, temp_folder_name, f"{str(i).zfill(6)}_{temp_folder_name}.jpg")
+                except Exception as e:
+                        print(f"An error occurred: {e}")
+
+
+                try:
+                    ret, imageToProcess = cam.read()
+                    datum.cvInputData = imageToProcess
+                    opWrapper.emplaceAndPop([datum])
+                    img = datum.cvOutputData.copy()
+                    if datum.poseKeypoints is not None and datum.poseKeypoints.shape == (1, 25, 3):
+
+                        full_25body_keypoints_data[i, :, :] = datum.poseKeypoints[0]
+                        full_25body_keypoints_data[i][full_25body_keypoints_data[i] == 0] = np.nan
+                    cv2.imwrite(filename, img)
+
+                except Exception as e:
+                    print(f"An error occurred: {e}")
+                    #img = 255 * np.ones((int(height), int(width), 3), np.uint8)
+                    #img = imageToProcess.copy()
+                    # Scale the font size based on the image's dimensions
+                    # Here, we scale it based on the width; you can adjust as needed
+                    fontScale = width / 1000.0  # This is an arbitrary scaling factor; you might want to adjust it
+
+                    # Get the size of the text box
+                    text = 'No detected person in this frame'
+                    (textWidth, textHeight), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale, 4)
+
+                    # Compute starting position for the text so it's centered
+                    x = int((width - textWidth) // 2)
+                    y = int((height + textHeight) // 2)
+
+                    # Place the text on the image
+                    # Place the text on the image
+                    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0, 0, 255), 2)
+                    cv2.imwrite(filename, img)
+                    print(f"Frame n.o.: {i} can't be processed.")
+
+            numpy_xyc_matrix = np.array(full_25body_keypoints_data)
+            print(f"Finished Openpose for file: {temp_folder_name}")
+
+            scipy.io.savemat(os.path.join(output_folder, f"{temp_folder_name}.mat"),
+                             mdict={'xbody': numpy_xyc_matrix[:, :, 0], 'ybody': numpy_xyc_matrix[:, :, 1], 'cbody': numpy_xyc_matrix[:, :, 2]})
+            np.save(os.path.join(output_folder, f"{temp_folder_name}.npy"), numpy_xyc_matrix)
+
+            df_xyc_25_keypoints = create_dataframe(numpy_xyc_matrix, KEYPOINTS_NAMES)
+            df_xyc_25_keypoints.to_csv(os.path.join(output_folder, f"{temp_folder_name}.csv"), index=False)
+            # Saving as HDF5 (more space efficient and faster for large datasets)
+            try:
+                df_xyc_25_keypoints.to_hdf(os.path.join(output_folder, f"{temp_folder_name}.h5"), key='keypoints', mode='w')
+            except:
+                print(" There is some problem with the H5 file creation ")
+            generate_video_from_frames(output_folder, temp_folder_name, frame_count, fps, platform)
         except:
-            print(" There is some problem with the H5 file creation ")
-        generate_video_from_frames(output_folder, temp_folder_name, frame_count, fps, platform)
+            print("Not working for: "+ name_of_file_and_exercise)
+            continue
+
 
 def main():
     args = get_arguments()
